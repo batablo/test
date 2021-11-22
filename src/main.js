1773f0dee23485181a2db34c52d38e3b8c639c3b
batablo <mutemath.us22@gmail.com>
2021-11-22 17:49:55 +0900

    add: render,$mountに関するコメント

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue';
 import App from './App.vue';
 
+// 開発モード警告文をconsoleで出すかどうか
 Vue.config.productionTip = false;
 
+/**
+ * https://qiita.com/teinen_qiita/items/ed1bb1909a17f9ca9daa
+ * hはhyperscriptの略で、仮想DOM実装時によく使われるらしい
+ * $mountはマウントするelementを渡す
+ */
 new Vue({
   render: (h) => h(App),
 }).$mount('#app');
