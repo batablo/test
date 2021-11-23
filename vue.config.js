96fd069db674ae343af994f46412d04069960c33
batablo <mutemath.us22@gmail.com>
2021-11-23 10:44:39 +0900

    add: グローバルSCSSの設定

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 module.exports = {
   // distのhtmlファイル内のパス設定(初期は'/')
   publicPath: '',
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: '@import "@/assets/sass/main.scss";',
+      },
+    },
+  },
 };
