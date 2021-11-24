a2a02c8f75bf75151f5b5bf91ddfe2a978a7ed9e
batablo <mutemath.us22@gmail.com>
2021-11-24 20:51:30 +0900

    add: vue router追加

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
+import router from './router';
 
 // 開発モード警告文をconsoleで出すかどうか
 Vue.config.productionTip = false;
@@ -10,5 +11,6 @@ Vue.config.productionTip = false;
  * $mountはマウントするelementを渡す
  */
 new Vue({
+  router,
   render: (h) => h(App),
 }).$mount('#app');
